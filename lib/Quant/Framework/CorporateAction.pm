2a157e11d39d2b41c5ea46db5f95168d93e9b6b2
Ivan Baidakou <ivan@binary.com>
2016-05-24 10:24:48 +0000

    Add documentation

--- lib/Quant/Framework/CorporateAction.pm
+++ lib/Quant/Framework/CorporateAction.pm
@@ -6,13 +6,54 @@ use warnings;
 use Date::Utility;
 use Quant::Framework::Document;
 use Moo;
+
 =head1 NAME
 
 Quant::Framework::CorporateAction
 
 =head1 DESCRIPTION
 
-Represents the corporate actions data of an underlying from database.
+Represents the corporate actions data of an underlying from database. Create
+new unpersistent CorporateAction can be done via:
+
+ my $ca = Quant::Framework::CorporateAction::create($storage_accessor, 'QWER', Date::Utility->new);
+
+Obviously, it will have empty actions hash
+
+ $ca->actions; # empty hashref by default
+
+Corporate actions can be persisted (stored in Chronicle) via C<save> method
+
+ $ca->save;
+
+Load persisted actions can be done via:
+
+ my $ca = Quant::Framework::CorporateAction::load($storage_accessor, 'QWER', Date::Utility->new);
+
+It will return C<undef> if there are no presisted actions in Chronicle. The date can be
+ommitted, than, it will try to load the most recent (last) corporate actions, i.e.
+
+ my $ca = Quant::Framework::CorporateAction::load($storage_accessor, 'QWER');
+
+
+To update actions, the C<update> method should be invoked, with appropriate structure, i.e.
+
+ my $ca = $ca->update({
+   "62799500" => {
+      "monitor_date" => "2014-02-07T06:00:07Z",
+      "type" => "ACQUIS",
+      "monitor" => 1,
+      "description" =>  "Acquisition",
+      "effective_date" =>  "15-Jul-14",
+      "flag" => "N",
+ }, Date::Utility->new);
+
+The C<update> in scalar context will return B<new unpersisted> Corporate object. You have
+to presist it via C<save> method.
+
+In the list context it will return new unpersisted Corporate object, and two hasref for
+new and cancelled action.
+
 
 =cut
 
