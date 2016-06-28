63e71902d6fb5da10ba1dd5ad9242fc31b77800e
Mahdi Mohammadi <mm@binary.com>
2016-06-28 08:40:21 +0000

    more strict caching

--- lib/Quant/Framework/TradingCalendar.pm
+++ lib/Quant/Framework/TradingCalendar.pm
@@ -1306,7 +1306,7 @@ sub weighted_days_in_period {
     my ($self, $begin, $end) = @_;
 
     state %cache;
-    my $key = $begin->epoch . $end->epoch . $self->underlying_config->quoted_currency_symbol . $self->underlying_config->asset_symbol . 
+    my $key = $begin->epoch . $end->epoch . $self->underlying_config->quoted_currency_symbol . $self->underlying_config->asset_symbol . $self->symbol . 
                     ($self->for_date ? $self->for_date->epoch:0);
 
     return $cache{$key} if defined $cache{$key};
